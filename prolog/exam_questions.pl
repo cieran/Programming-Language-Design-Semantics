717cbcf783e4b2c2519f5bb6904986fdf5fc02f3
Cieran Neeson <cieranneeson@gmail.com>
2017-01-09 23:44:14 +0000

    gitignore setup

--- prolog/exam_questions.pl
+++ prolog/exam_questions.pl
@@ -10,20 +10,24 @@ tr(M, [Hs|M1]) :- maplist(head, M, Hs), maplist(tail, M, Ts),
 
 /* January 2015 */
 doublemember(_,[]).
-doublemember(X, [H|T]) :-  testfunction(X,[H|T],0).
- 
-testFunction(_,[],_).
-testfunction(_,[],Z) :- (Z>=2). 
-testfunction(X,[H|T],Z) :- 
+doublemember(X, [H|T]) :-  counter(X,[H|T],0).
+counter(_,[],Z) :- (Z>=2). 
+counter(X,[H|T],Z) :- 
 	(X = H) -> 
-		(Q=Z+1), testfunction(X,T,Q)
+		(Q is Z+1 ),  counter(X,T,Q)
 	; 
-	testfunction(X,T,Z).
-
-
+	counter(X,T,Z).
+/* January 2015 Manus */
+doublemember2(X,Xs) :-
+	member(X,Xs),
+	deletemember2(X,Xs,N), /*removes from list*/
+	member(X,N).
+deletemember2(X,[X|T],T).
+deletemember2(X,[Y|T],[Y|T1]):- del(X,T,T1).
 /* January 2014
 	noahDouble([1,2,3,4],[[1,2],[3,4]]).
 */
 noahDouble([],[]).
 noahDouble([H1, H2 | T], [[H1, H2]| T2]) :-
 	noahDouble(T, T2).
+
