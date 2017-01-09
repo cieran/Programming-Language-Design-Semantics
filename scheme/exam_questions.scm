eaa7588fc066e6ae054c123e09b464588792bab1
Cieran Neeson <cieranneeson@gmail.com>
2017-01-09 21:59:45 +0000

    First Commit

--- scheme/exam_questions.scm
+++ scheme/exam_questions.scm
@@ -1,3 +1,4 @@
+; 2015 January
 (define (reverse-with-count xs ys))
 	(reverse (range-list xs ys))
 
@@ -9,11 +10,32 @@
 
 
 
-;
 ;	(tr '((1 2 3) (4 5 6)))
 ;		=> ((1 4) (2 5) (3 6))
 ;	(tr '((f o x e s) (s o c k s) (r o c k s)))
 ;		=> ((f s r) (o o o) (x c c) (e k k) (s s s))
 
+(define (tr xs)
+  (apply map list xs))
 
 
+(define (after-filter predicate xs)
+    (cond [(<(length xs)2) '()]                 ;;if input is NULL or length<2 => do nothing
+          [else
+           (cond [(predicate (car xs))                                ;;check first element with predicate
+                  (cons (cadr xs) (after-filter predicate (cdr xs)))] ;;if true, store element to it's right and recursively call function on tail
+                 [else (after-filter predicate (cdr xs))])]))
+
+
+; 2014 Autumn
+(define (add-numbers xs)
+  (sum-them (flatten xs)))
+
+(define (sum-them xs)
+  (apply + (find-nums xs)))
+
+(define (find-nums xs)
+  (cond [(<(length xs)2) '()]
+         [else (cond [(number? (car xs))
+         (cons (car xs) (find-nums (cdr xs)))]
+        [else (find-nums (cdr xs))])]))
